982c172719e56dde85a577e19ce1aeccadc2e8e5
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-21 22:41:48 +0200

    lets see the payment

--- pages/api/payment-status.js
+++ pages/api/payment-status.js
@@ -2,6 +2,7 @@ const { createMollieClient } = require('@mollie/api-client');
 const mollieClient = createMollieClient({ apiKey: "test_6AVPHyeQADq34cfPN3uUU85EAPrxxh" });
 
 export default async (req, res) => {
-    console.log(req.body)
+    const payment = await mollieClient.payments.get(req.body.id)
+    console.log(payment)
     res.json({status: 200})
 }
