9ede47b7e5d92577d2b813e5f4aedd2d264789f9
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-21 22:29:06 +0200

    view post request on webhook

--- pages/api/payment-status.js
+++ pages/api/payment-status.js
@@ -2,6 +2,6 @@ const { createMollieClient } = require('@mollie/api-client');
 const mollieClient = createMollieClient({ apiKey: "test_6AVPHyeQADq34cfPN3uUU85EAPrxxh" });
 
 export default async (req, res) => {
-    console.log(req.id)
+    console.log(req)
     res.json({status: 200})
 }
