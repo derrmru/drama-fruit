5adc54da26ed4f60d2dbc6bb119c1660eb97a399
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-22 10:50:12 +0200

    remove duplicate default export

--- pages/payment-complete.js
+++ pages/payment-complete.js
@@ -1,6 +1,3 @@
-const { createMollieClient } = require('@mollie/api-client');
-const mollieClient = createMollieClient({ apiKey: "test_6AVPHyeQADq34cfPN3uUU85EAPrxxh" });
-
 const PaymentComplete = (props) => {
     console.log(props)
     return (
@@ -11,14 +8,3 @@ const PaymentComplete = (props) => {
 }
 
 export default PaymentComplete
-
-
-export async function getServerSideProps() {
-    // Fetch data from external API
-    const data = await req
-  
-    // Pass data to the page via props
-    return { props: { data } }
-  }
-  
-  export default Page
