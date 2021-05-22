32bc18079e1dcd26f937b51b4ceaca3a1d3dc481
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-22 11:00:28 +0200

    remove update, not possible for complete transactions

--- pages/api/payment-status.js
+++ pages/api/payment-status.js
@@ -3,9 +3,7 @@ const mollieClient = createMollieClient({ apiKey: "test_6AVPHyeQADq34cfPN3uUU85E
 
 export default async (req, res) => {
     //after transaction is complete we receive payment ID and must query the status
-    const payment = await mollieClient.payments.update(req.body.id, {
-        redirectUrl: 'https://drama-fruit.vercel.app/payment-complete'
-    })
-    console.log(payment.status)
+    const payment = await mollieClient.payments.get(req.body.id)
+    console.log(payment)
     res.status(200)
 }
