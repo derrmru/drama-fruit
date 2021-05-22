6a008db2022c2f9683b28a695ab50a132a8ac35f
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-22 10:57:03 +0200

    remove equals

--- pages/api/payment-status.js
+++ pages/api/payment-status.js
@@ -4,7 +4,7 @@ const mollieClient = createMollieClient({ apiKey: "test_6AVPHyeQADq34cfPN3uUU85E
 export default async (req, res) => {
     //after transaction is complete we receive payment ID and must query the status
     const payment = await mollieClient.payments.update(req.body.id, {
-        redirectUrl = 'https://drama-fruit.vercel.app/payment-complete'
+        redirectUrl: 'https://drama-fruit.vercel.app/payment-complete'
     })
     console.log(payment.status)
     res.status(200)
