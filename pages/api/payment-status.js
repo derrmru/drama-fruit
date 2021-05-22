a5770c0db55919695f3c4bdd1125526aadff4f51
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-22 10:53:11 +0200

    stuff

--- pages/api/payment-status.js
+++ pages/api/payment-status.js
@@ -5,8 +5,7 @@ export default async (req, res) => {
     //after transaction is complete we receive payment ID and must query the status
     const payment = await mollieClient.payments.get(req.body.id)
     console.log(payment.status)
-    payment.update({
-        redirectUrl: 'https://drama-fruit.vercel.app/payment-complete'
-    })
+    payment.redirectUrl = 'https://drama-fruit.vercel.app/payment-complete'
+    payment.update()
     res.status(200)
 }
