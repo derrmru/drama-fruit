9b384902e294a734254505d18704d89ceb5b1435
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-22 10:56:08 +0200

    try to restructure method

--- pages/api/payment-status.js
+++ pages/api/payment-status.js
@@ -3,9 +3,9 @@ const mollieClient = createMollieClient({ apiKey: "test_6AVPHyeQADq34cfPN3uUU85E
 
 export default async (req, res) => {
     //after transaction is complete we receive payment ID and must query the status
-    const payment = await mollieClient.payments.get(req.body.id)
+    const payment = await mollieClient.payments.update(req.body.id, {
+        redirectUrl = 'https://drama-fruit.vercel.app/payment-complete'
+    })
     console.log(payment.status)
-    payment.redirectUrl = 'https://drama-fruit.vercel.app/payment-complete'
-    payment.update()
     res.status(200)
 }
