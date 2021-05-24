bc5478c6ec020998ff1b19e486699b0acf72705e
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-24 11:08:05 +0200

    transaction status statements

--- pages/payment-complete.js
+++ pages/payment-complete.js
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import { useEffect, useState } from "react"
 import $ from 'jquery'
+import style from '../styles/Payment.module.css'
 
 const PaymentComplete = () => {
     const [transaction, setTransaction] = useState();
@@ -8,7 +10,6 @@ const PaymentComplete = () => {
     let tId;
     useEffect(() => {
         tId = window.location.href.split('transaction=')[1];
-        console.log(tId)
 
         //get order status from transaction id using api/payment-check
         $.post(
@@ -16,15 +17,53 @@ const PaymentComplete = () => {
             { 
                 transaction_id: tId 
             }).done((data) => {
+                console.log(data)
                 setTransaction(data)
             })
     }, [])
 
-    console.log(transaction)
-
     return (
         <div>
-            Payment Complete
+            <Head>
+                <title>Drama Fruit</title>
+                <meta name="description" content="Successful Transaction" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Layout>
+                <div className={style.response}>
+                {
+                    transaction["status"] === 'paid' ? <div>
+                        <h2 style={{ textAlign: 'center', margin: '40px 0 40px' }}>Payment Complete!</h2>
+                        <h3 style={{ textAlign: 'center', margin: '40px 0 40px' }}>Please check your emails for confirmation of your order.</h3>
+                        <h3 style={{ textAlign: 'center', margin: '40px 0 40px' }}>Your Order ID is: <span style={{fontSize: '28px'}}>{tId}</span></h3>
+                        <div style={{ textAlign: 'center', margin: '40px 0 40px' }}>Feel like buying more?! Head back to the <Link href="/drama-shop"><a>Shop</a></Link></div>
+                    </div> :
+                        transaction["status"] === 'open' ? <div>
+                        <h2 style={{ textAlign: 'center', margin: '40px 0 40px' }}>Payment Incomplete!</h2>
+                        <h3 style={{ textAlign: 'center', margin: '40px 0 40px' }}>It looks like you started your payment, but it didn't finish.</h3>
+                        <div style={{ textAlign: 'center', margin: '40px 0 40px' }}>Please head back to the <Link href="/drama-shop"><a>Shop</a></Link> and try again.</div>
+                    </div> :
+                        transaction["status"] === 'expired' ? <div>
+                        <h2 style={{ textAlign: 'center', margin: '40px 0 40px' }}>Payment Incomplete!</h2>
+                        <h3 style={{ textAlign: 'center', margin: '40px 0 40px' }}>It looks like you started your payment, but you didn't finish in time.</h3>
+                        <div style={{ textAlign: 'center', margin: '40px 0 40px' }}>Please head back to the <Link href="/drama-shop"><a>Shop</a></Link> and try again.</div>
+                    </div> :
+                        transaction["status"] === 'canceled' ? <div>
+                        <h2 style={{ textAlign: 'center', margin: '40px 0 40px' }}>Payment Canceled!</h2>
+                        <h3 style={{ textAlign: 'center', margin: '40px 0 40px' }}>It looks like you had second thoughts.</h3>
+                        <div style={{ textAlign: 'center', margin: '40px 0 40px' }}>Why not head back to the <Link href="/drama-shop"><a>Shop</a></Link> so I can convince you!</div>
+                    </div> :
+                    transaction["status"] === 'failed' ? <div>
+                        <h2 style={{ textAlign: 'center', margin: '40px 0 40px' }}>Payment Failed!</h2>
+                        <h3 style={{ textAlign: 'center', margin: '40px 0 40px' }}>It looks like we can't accept your payment method.</h3>
+                        <div style={{ textAlign: 'center', margin: '40px 0 40px' }}>Why not head back to the <Link href="/drama-shop"><a>Shop</a></Link> and try again with a different card.</div>
+                    </div> :
+                        <div>
+                            Something strange occured, please contact us!
+                        </div>
+                }
+                </div>
+            </Layout>
         </div>
     )
 }
