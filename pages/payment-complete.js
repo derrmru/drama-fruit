8301c1ccdef7b7b0f736683544466b13aef826aa
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-24 11:24:50 +0200

    include layout in completion page

--- pages/payment-complete.js
+++ pages/payment-complete.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../components/templates/Layout'
 import { useEffect, useState } from "react"
 import $ from 'jquery'
 import style from '../styles/Payment.module.css'
