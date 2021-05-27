66a5ac60bada0ccf13cbefb4e286f7cda79feafd
derrmru <podogomacbookair@MacBook-Air.local>
2021-05-27 10:46:45 +0200

    push honeypot to back of frame

--- components/form_components/TextInput.js
+++ components/form_components/TextInput.js
@@ -10,7 +10,7 @@ const TextInput = ({
     return (
         <label 
             className={style.label} 
-            style={name === 'oh_no_honey' ? {opacity: 0, position: 'fixed', top: 0} : {}}
+            style={name === 'oh_no_honey' ? {opacity: 0, position: 'fixed', top: 0, left: 0, zIndex: '-1'} : {}}
             >
             {name.split('_').map(word => ' ' + word[0].toUpperCase() + word.toLowerCase().slice(1))}:
             <br />
