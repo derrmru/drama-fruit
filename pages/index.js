d57b76f4f6ebfbac98b5bd472af9a81ca9bdabf2
derrmru <podogomacbookair@MacBook-Air.local>
2021-08-25 17:04:24 +0200

    apple touch icons included for home

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,10 @@ const Home = ({ items }) => {
         <meta name="description" content={items && items.fields.seoDescription} />
         <link rel="icon" href="/favicon.ico" />
         <link rel="canonical" href="https://www.dramafruit.com/" />
+        <link rel="apple-touch-icon" href="/images/apple-touch-icons/apple-touch-icon-iphone-60x60.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/images/apple-touch-icons/somedir/apple-touch-icon-ipad-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-touch-icons/somedir/apple-touch-icon-iphone-retina-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/images/apple-touch-icons/somedir/apple-touch-icon-ipad-retina-152x152.png" />
         <script type="application/ld+json">{org()}</script>
       </Head>
       <Layout>
