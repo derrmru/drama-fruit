6dc0150e06ac79d8527439b06781fd23a4481084
derrmru <peter@thepetersweeney.com>
2021-08-31 13:42:51 +0200

    remove inline style from homepage

--- components/Home/BlogCase.js
+++ components/Home/BlogCase.js
@@ -7,24 +7,18 @@ const ShopCase = ({
 }) => {
     return (
         <div className={style.bannerContainer + ' fade-in ' + style.blogReverse}>
-            <div 
-                className={style.bannerText + ' ' + style.blogText}
-                style={{borderColor: 'var(--drama-yellow)'}}
-                >
+            <div className={style.bannerText + ' ' + style.blogText}>
                 <div>
                     {documentToReactComponents(items && items.fields.blogText)}
                     <Link href="/past-projects"><a>Blog &#8594;</a></Link>
                 </div>
             </div>
-            <div 
-                className={style.bannerImage + ' ' + style.blogImage}
-                style={{borderColor: 'var(--drama-yellow)'}}
-                >
+            <div className={style.bannerImage + ' ' + style.blogImage}>
                 <img
-                    className={style.theImage} 
-                    src={items && items.fields.blogImage.fields.file.url} 
+                    className={style.theImage}
+                    src={items && items.fields.blogImage.fields.file.url}
                     alt={items && items.fields.blogAltDescription}
-                    />
+                />
             </div>
         </div>
     )
--- components/Home/ShopCase.js
+++ components/Home/ShopCase.js
@@ -7,20 +7,14 @@ const ShopCase = ({
 }) => {
     return (
         <div className={style.bannerContainer + ' fade-in ' + style.shopContainer}>
-            <div 
-                className={style.bannerImage + ' ' + style.shopImage}
-                style={{borderColor: 'var(--drama-pink)'}}
-                >
+            <div className={style.bannerImage + ' ' + style.shopImage}>
                 <img
-                    className={style.theImage} 
-                    src={items && items.fields.shopImage.fields.file.url} 
+                    className={style.theImage}
+                    src={items && items.fields.shopImage.fields.file.url}
                     alt={items && items.fields.shopAltDescription}
-                    />
+                />
             </div>
-            <div 
-                className={style.bannerText + ' ' + style.shopText}
-                style={{borderColor: 'var(--drama-pink)'}}
-                >
+            <div className={style.bannerText + ' ' + style.shopText}>
                 <div>
                     {documentToReactComponents(items && items.fields.shopText)}
                     <Link href="/drama-shop"><a>Shop &#8594;</a></Link>
